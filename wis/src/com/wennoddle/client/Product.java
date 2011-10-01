d4a791defa13749f7c8c1f394a8b7f85b6abd6b5
Jake Chan <jakechankit@gmail.com>
2011-10-02 03:18:44 +0800

    Testing

--- wis/src/com/wennoddle/client/Product.java
+++ wis/src/com/wennoddle/client/Product.java
@@ -5,4 +5,40 @@ public class Product {
 	private String price;
 	private String unit;
 	private String remark;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public Product(String name, String price, String unit, String remark) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.unit = unit;
+		this.remark = remark;
+	}
+	
+	
+	
 }
--- wis/src/com/wennoddle/client/Wis.java
+++ wis/src/com/wennoddle/client/Wis.java
@@ -1,14 +1,14 @@
 package com.wennoddle.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.MenuBar;
-import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -26,7 +26,7 @@ public class Wis implements EntryPoint {
 
 	private void createView() {
 		RootPanel.get("systemMenu").add(createMenu());
-		RootPanel.get("systemBody").add(new HTML("contentPage"));
+		RootPanel.get("systemBody").add(createSampleBody());
 		
 	}
 
@@ -67,7 +67,62 @@ public class Wis implements EntryPoint {
 	}
 	
 	private Widget createSampleBody() {
-		CellTable<Product> productCellTable = new CellTable<Product>(); 
-		return null;
+		CellTable<Product> productList = new CellTable<Product>();
+		
+		TextColumn<Product> nameColumn = new TextColumn<Product>(){
+
+			@Override
+			public String getValue(Product product) {
+				return product.getName();
+			}
+			
+		};
+		
+		TextColumn<Product> priceColumn = new TextColumn<Product>(){
+
+			@Override
+			public String getValue(Product product) {
+				return product.getPrice();
+			}
+			
+		};
+		
+		TextColumn<Product> unitColumn = new TextColumn<Product>(){
+
+			@Override
+			public String getValue(Product product) {
+				return product.getUnit();
+			}
+			
+		};
+		
+		TextColumn<Product> remarkColumn = new TextColumn<Product>(){
+
+			@Override
+			public String getValue(Product product) {
+				return product.getPrice();
+			}
+			
+		};
+		
+		nameColumn.setSortable(true);
+		priceColumn.setSortable(true);
+		unitColumn.setSortable(true);
+		remarkColumn.setSortable(true);
+		
+
+		productList.addColumn(nameColumn);
+		productList.addColumn(priceColumn);
+		productList.addColumn(unitColumn);
+		productList.addColumn(remarkColumn);
+		
+		List<Product> products = new ArrayList<Product>();
+		products.add(new Product("123","123", "123", "123"));
+		productList.setRowData(products);
+		productList.setWidth("100%", true);
+		productList.setRowCount(products.size(), true);
+		
+		
+		return productList;
 	}
 }
